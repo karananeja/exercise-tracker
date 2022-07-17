e90708dca20cf79ae265becd3a9ca51b3266b414
KARAN KUMAR ANEJA R <karananeja.r@gmail.com>
2022-07-17 18:49:48 +0530

    backend hosted

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://rt-exercise-tracker.herokuapp.com',
 });
 
 export default instance;
