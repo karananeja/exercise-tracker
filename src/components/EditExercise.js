1b52474d3d21c08acdde120919b0f874e521ffe5
KARAN KUMAR ANEJA R <karananeja.r@gmail.com>
2022-07-17 18:34:58 +0530

    backend hosted

--- src/components/EditExercise.js
+++ src/components/EditExercise.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from './axios';
+import axios from '../axios';
 import { useParams } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
